246e04c3fe7426293ba8d6d4aafead93ac81b18a
Richard Terry <rterry@internode.on.net>
2010-12-15 09:20:48 +0000

    debugging again FMetadata as it was no longer working after blob changes.

--- client/config/0.54_update_lu_audit_action.sql
+++ client/config/0.54_update_lu_audit_action.sql
@@ -1,7 +1,12 @@
 update clin_consult.lu_audit_actions set action = 'viewed and filed document' where pk = 21
 insert into clin_consult.lu_audit_actions  (action) values ('note on task');
- insert into clin_consult.lu_audit_actions (action) values ('task component finalised');
- Insert into clin_consult.lu_audit_actions (action) values ('scanned document imported');
+insert into clin_consult.lu_audit_actions (action) values ('task component finalised');
+Insert into clin_consult.lu_audit_actions (action) values ('scanned document imported');
+ 
+Update clin_consult.lu_audit_actions set action='insert' where pk = 1;
+Update clin_consult.lu_audit_actions set action='edit' where pk = 2;
+Update clin_consult.lu_audit_actions set action='delete' where pk = 3;
+insert into clin_consult.lu_audit_actions(action) values ('re-scheduled');
  
 truncate db.lu_version;
 insert into db.lu_version (lu_major,lu_minor) values (0, 54)

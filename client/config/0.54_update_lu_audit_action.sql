2b0c4e9dd77a829851eadcc0a63a002cb2ace80a
Richard Terry <rterry@internode.on.net>
2010-12-15 05:06:40 +0000

    New form FGraph to allowing viewing cumulative pathology results in the inbox Fixed incorrect labelling of Sender Name/User Name in Inbox New Icon for the inbox with a visual notification of inbox having contents which will be implemented later.

--- client/config/0.54_update_lu_audit_action.sql
+++ client/config/0.54_update_lu_audit_action.sql
@@ -1,7 +1,7 @@
 update clin_consult.lu_audit_actions set action = 'viewed and filed document' where pk = 21
 insert into clin_consult.lu_audit_actions  (action) values ('note on task');
  insert into clin_consult.lu_audit_actions (action) values ('task component finalised');
- 
+ Insert into clin_consult.lu_audit_actions (action) values ('scanned document imported');
  
 truncate db.lu_version;
 insert into db.lu_version (lu_major,lu_minor) values (0, 54)

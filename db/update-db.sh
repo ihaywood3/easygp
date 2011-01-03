9bcbdef7aab766b2025eac66e2f5f6de56dac07b
Ian Haywood <ian@haywood.id.au>
2011-01-03 10:19:18 +0000

    removed extra grant commands

--- db/update-db.sh
+++ db/update-db.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-MAJOR=`psql -tA -c "select lu_major from db.lu_version" easygp`
-MINOR=`psql -tA -c "select lu_minor from db.lu_version" easygp`
+if [ -n $1 ] ; then CONN=$1 ; else CONN=easygp ; fi
+
+MAJOR=`psql -tA -c "select lu_major from db.lu_version" $CONN`
+MINOR=`psql -tA -c "select lu_minor from db.lu_version" $CONN`
 
 cd updates
 i=$MINOR
 let i++
 
 while [ -e $MAJOR.$i*.sql ] ; do
-   psql -f $MAJOR.$i*.sql easygp
+   psql -f $MAJOR.$i*.sql $CONN
    let i++
 done

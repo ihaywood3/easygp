42121c0e0ac10610d3a2b74687d07efbf8f80d24
Ian Haywood <ian@haywood.id.au>
2011-01-16 04:32:32 +0000

    database updates must be sequential

--- db/updates/0.66_add_dose_as_unit.sql
+++ db/updates/0.66_add_dose_as_unit.sql
@@ -1,4 +1,4 @@
 insert into common.lu_units (pk, abbrev_text, full_text) values (56, 'dose', 'dose');
 
 truncate table db.lu_version;
-insert into db.lu_version (lu_major,lu_minor) values (0, 62);
+insert into db.lu_version (lu_major,lu_minor) values (0, 66);

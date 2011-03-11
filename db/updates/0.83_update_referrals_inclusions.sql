08036d310f8100bc8ca650bfb17c8832daa98ff4
Richard Terry <rterry@internode.on.net>
2011-03-11 11:37:33 +0000

    Fworkcover   [bugfix] Forms saved without a coded diagnosis now don't crash on starting a new visit for the claim   [new   ] Global preference flag bInsistCoding included (not implemented) default false 	   Will allow sysadmin to insist on a coded diagnosis if that is what the surgery wants 	   This feature will be an option when admin.global preferences finally done.
    Finbox
      [bugfix] Program no longer crashes when auto-graph shows buttons for abnormals which have not been supplied with  loincs (hopefully, to be tested in practice)
    
    
    FReferrals
    	Implemented re-showing inclusions for referrals printed 'today' next to the document list under the editing area
    
    FDACC
    	Found yet another variation in LOINC for urinary protein testing for diabetes - included
    
    this line, and those below, will be ignored--
    
    M    trunk/db/updates/0.83_update_referrals_inclusions.sql
    M    trunk/client/.startup
    M    trunk/client/.project
    M    trunk/client/.src/inbox/FInbox.class
    M    trunk/client/.src/common/const.module
    M    trunk/client/.src/clinical/referrals/FReferrals.class
    M    trunk/client/.src/clinical/referrals/modReferralsDBI.module
    M    trunk/client/.src/clinical/referrals/FReferrals.form
    M    trunk/client/.src/clinical/FGraph.class
    M    trunk/client/.src/clinical/diabetes/FDACC.class
    M    trunk/client/.src/clinical/workcover/FWorkCover.class
    M    trunk/client/.src/clinical/notes/FProgressNoteEditor.form

--- db/updates/0.83_update_referrals_inclusions.sql
+++ db/updates/0.83_update_referrals_inclusions.sql
@@ -1,16 +1,35 @@
 -- some minor mods and ownership changes to inclusions;
-
-alter table  clin_referrals.inclusions add column  deleted boolean default false;
-
-comment on column clin_referrals.inclusions.deleted is
-'if deleted is true then the inclusion is marked as deleted and
- for example will not be sent out if the document is later re-printed';
-
-
+CREATE TABLE clin_referrals.inclusions
+(
+  pk serial NOT NULL,
+  fk_referral integer NOT NULL,
+  fk_document integer NOT NULL,
+  deleted boolean DEFAULT false, -- if deleted is true then the inclusion is marked as deleted and...
+  CONSTRAINT inclusions_pkey PRIMARY KEY (pk)
+)
+WITH (
+  OIDS=FALSE
+);
 ALTER TABLE clin_referrals.inclusions OWNER TO easygp;
 GRANT ALL ON TABLE clin_referrals.inclusions TO easygp;
 GRANT SELECT ON TABLE clin_referrals.inclusions TO staff;
- 
+COMMENT ON TABLE clin_referrals.inclusions IS 'A Table describing which documents went out with the referral';
+COMMENT ON COLUMN clin_referrals.inclusions.deleted IS 'if deleted is true then the inclusion is marked as deleted and
+ for example will not be sent out if the document is later re-printed';
+
+
+CREATE OR REPLACE VIEW clin_referrals.vwinclusions AS 
+ SELECT DISTINCT inclusions.pk AS pk_inclusion, inclusions.fk_referral, inclusions.fk_document, inclusions.deleted, consult.fk_patient, documents.date_created, documents.tag_user
+   FROM clin_consult.consult
+   JOIN clin_referrals.referrals ON referrals.fk_consult = consult.pk
+   JOIN clin_referrals.inclusions ON referrals.pk = inclusions.fk_referral
+   JOIN documents.documents ON inclusions.fk_document = documents.pk
+  ORDER BY consult.fk_patient;
+
+ALTER TABLE clin_referrals.vwinclusions OWNER TO easygp;
+GRANT ALL ON TABLE clin_referrals.vwinclusions TO easygp;
+GRANT ALL ON TABLE clin_referrals.vwinclusions TO staff;
+
 
 truncate db.lu_version;
 insert into db.lu_version (lu_major,lu_minor) values (0, 83);

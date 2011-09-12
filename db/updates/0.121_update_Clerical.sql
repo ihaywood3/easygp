e0e71a92a2c730f11fc347607f037115323b98b0
Richard Terry <rterry@internode.on.net>
2011-09-12 14:06:11 +0000

    work on appointment schedules for staff membmers

---
+++ db/updates/0.121_update_Clerical.sql
@@ -0,0 +1,9 @@
+-- minor modifications to appointments
+
+Alter table clerical.sessions add column appointment_interval interval default '10 minutes';
+alter table clerical.bookings rename column booked_by to fk_staff_booked;
+alter table clerical.bookings rename column booked_at to fk_clinic;
+
+truncate db.lu_version;
+insert into db.lu_version (lu_major,lu_minor) values (0, 115);
+

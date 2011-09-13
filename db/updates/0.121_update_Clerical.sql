0b8c53649633f3dd420ce1692564a0b0920a6fc1
Richard Terry <rterry@internode.on.net>
2011-09-13 13:06:49 +0000

    work on appointments

--- db/updates/0.121_update_Clerical.sql
+++ db/updates/0.121_update_Clerical.sql
@@ -2,8 +2,9 @@
 
 Alter table clerical.sessions add column appointment_interval interval default '10 minutes';
 alter table clerical.bookings rename column booked_by to fk_staff_booked;
-alter table clerical.bookings rename column booked_at to fk_clinic;
-
+ALTER TABLE clerical.bookings drop column fk_clinic;
+alter table clerical.bookings add column fk_clinic integer not null;
+alter table clerical.bookings add deleted boolean default false;
 truncate db.lu_version;
 insert into db.lu_version (lu_major,lu_minor) values (0, 115);
 

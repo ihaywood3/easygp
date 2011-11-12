1e74843a540db41e4edb64a7c8e906e1e48724f4
Richard Terry <rterry@internode.on.net>
2011-11-12 13:14:00 +0000

    ....some changes

---
+++ db/updates/0.135_update_medical_certificats.sql
@@ -0,0 +1,40 @@
+-- added latex definition of the certificate - no default as I have already written many medical certificates.
+
+alter table clin_certificates.medical_certificates add column latex text;
+
+COMMENT ON COLUMN clin_certificates.medical_certificates.latex is
+ 'the latex text of the certificate';
+
+DROP VIEW clin_certificates.vwmedicalcertificates;
+
+CREATE OR REPLACE VIEW clin_certificates.vwmedicalcertificates AS 
+ SELECT medical_certificates.pk AS pk_medicalcertificate, consult.fk_patient, consult.consult_date, 
+ medical_certificates.fk_consult, medical_certificates.certificate_date, medical_certificates.reason, 
+ medical_certificates.fk_coding_system, medical_certificates.fk_code, medical_certificates.fk_lu_illness_temporality, 
+ medical_certificates.fk_lu_fitness, lu_fitness.fitness, medical_certificates.from_date, medical_certificates.deleted, 
+ medical_certificates.to_date, medical_certificates.notes, medical_certificates.fk_progressnote, medical_certificates.latex, consult.fk_staff, 
+ vwstaffinclinics.wholename AS staff_wholename, vwstaffinclinics.title AS staff_title, vwstaffinclinics.branch AS staff_branch, 
+ vwstaffinclinics.organisation AS staff_organisation, vwstaffinclinics.street1 AS staff_street1, vwstaffinclinics.street2 AS staff_street2, 
+ vwstaffinclinics.town AS staff_town, vwstaffinclinics.postcode AS staff_postcode, vwstaffinclinics.provider_number AS staff_provider_number, 
+ lu_illness_temporality.temporality, lu_systems.system, generic_terms.term, generic_terms.code
+   FROM clin_certificates.medical_certificates medical_certificates
+   JOIN clin_consult.consult ON medical_certificates.fk_consult = consult.pk
+   JOIN admin.vwstaffinclinics ON consult.fk_staff = vwstaffinclinics.fk_staff
+   JOIN clin_certificates.lu_illness_temporality ON medical_certificates.fk_lu_illness_temporality = lu_illness_temporality.pk
+   JOIN clin_certificates.lu_fitness ON medical_certificates.fk_lu_fitness = lu_fitness.pk
+   LEFT JOIN coding.lu_systems ON medical_certificates.fk_coding_system = lu_systems.pk
+   LEFT JOIN coding.generic_terms ON medical_certificates.fk_code = generic_terms.code
+  WHERE medical_certificates.deleted = false
+  ORDER BY consult.fk_patient, consult.consult_date;
+
+ALTER TABLE clin_certificates.vwmedicalcertificates OWNER TO easygp;
+GRANT ALL ON TABLE clin_certificates.vwmedicalcertificates TO easygp;
+GRANT ALL ON TABLE clin_certificates.vwmedicalcertificates TO staff;
+COMMENT ON VIEW clin_certificates.vwmedicalcertificates IS 'A view of patients medical certificate history, includes written by which staff member and where';
+
+
+
+
+truncate db.lu_version;
+insert into db.lu_version (lu_major,lu_minor) values (0, 135);
+
---
+++ db/updates/0.136_update_clin_workcover.sql
@@ -0,0 +1,40 @@
+-- new field to keep the latex of the certificate
+alter table clin_workcover.visits add column latex text;
+comment on column clin_workcover.visits.latex is 'the LaTex definition of the workcover form';
+
+DROP VIEW clin_workcover.vwworkcover;
+
+CREATE OR REPLACE VIEW clin_workcover.vwworkcover AS 
+ SELECT visits.pk AS pk_view, visits.pk AS fk_visit, visits.fk_claim, consult1.consult_date AS start_date, consult.consult_date AS visit_date, consult.fk_patient, claims.claim_number, claims.fk_occupation, claims.fk_branch, claims.hours_week_worked, claims.mechanism_of_injury, claims.date_injury, claims.contact_person, claims.memo, claims.identifier, claims.fk_person, claims.accepted, consult.fk_staff, consult.fk_type, consult.summary, vwstaff.wholename AS staff_wholename, vwstaff.surname AS staff_surname, vwstaff.firstname AS staff_firstname, vwstaff.title AS staff_title, vwstaff.provider_number, lu_occupations.occupation, vworganisations.branch, vworganisations.fk_organisation, vworganisations.organisation, vworganisations.street1 AS branch_street1, vworganisations.street2 AS branch_street2, vworganisations.town AS branch_town, vworganisations.branch_deleted, vwpersons.wholename AS soletrader_wholename, vwpersons.firstname AS soletrader_firstname, vwpersons.surname AS soletrader_surname, vwpersons.title AS soletrader_title, vwpersons.town AS soletrader_town, vwpersons.street1 AS soletrader_street1, vwpersons.street2 AS soletrader_street2, vwpersons.postcode AS soletrader_postcode, vwpersons.address_deleted AS soletrader_address_deleted, lu_visit_type.type AS visit_type, visits.fk_lu_visit_type, visits.diagnosis, lu_systems.system AS coding_system, visits.fk_code, 
+        CASE
+            WHEN visits.fk_code IS NOT NULL THEN ( SELECT DISTINCT generic_terms.term
+               FROM coding.generic_terms
+              WHERE visits.fk_code = generic_terms.code)
+            ELSE NULL::text
+        END AS coded_term, visits.certificate_date, visits.management_plan, visits.review_date,
+         visits.assessworkplace, visits.hours_capable, visits.days_capable, visits.restrictions, visits.fk_caused_by_employment,
+          visits.doctor_consented, visits.worker_consented, visits.fitness_preinjury_from, visits.fitness_suitable_from, visits.fitness_suitable_to, 
+          visits.fitness_unfit_from, visits.fitness_unfit_to, visits.fitness_perm_mod_duties_from, visits.fk_consult, 
+          visits.fk_progressnote, visits.fk_coding_system, visits.capabilities, visits.latex, lu_caused_by_employment.caused_by_employment
+   FROM clin_consult.consult
+   JOIN admin.vwstaff ON consult.fk_staff = vwstaff.fk_staff
+   JOIN clin_workcover.visits ON visits.fk_consult = consult.pk
+   JOIN clin_workcover.claims ON claims.pk = visits.fk_claim
+   JOIN common.lu_occupations ON claims.fk_occupation = lu_occupations.pk
+   LEFT JOIN coding.lu_systems ON visits.fk_coding_system = lu_systems.pk
+   LEFT JOIN contacts.vworganisations ON claims.fk_branch = vworganisations.fk_branch
+   LEFT JOIN contacts.vwpersons ON claims.fk_person = vwpersons.fk_person
+   JOIN clin_workcover.lu_visit_type ON visits.fk_lu_visit_type = lu_visit_type.pk
+   JOIN clin_workcover.lu_caused_by_employment ON visits.fk_caused_by_employment = lu_caused_by_employment.pk
+   JOIN clin_consult.consult consult1 ON claims.fk_consult = consult1.pk
+  ORDER BY visits.fk_claim, visits.pk;
+
+ALTER TABLE clin_workcover.vwworkcover OWNER TO easygp;
+GRANT ALL ON TABLE clin_workcover.vwworkcover TO easygp;
+GRANT SELECT ON TABLE clin_workcover.vwworkcover TO staff;
+COMMENT ON VIEW clin_workcover.vwworkcover IS 'View of all visits for all claims date ordered. If the work cover form was coded also contains the coding system
+ the coded term and the code';
+
+
+truncate db.lu_version;
+insert into db.lu_version (lu_major,lu_minor) values (0, 136);

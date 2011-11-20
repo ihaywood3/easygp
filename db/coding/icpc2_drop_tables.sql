8b1724b1d20582766c2b67c4197a74b24b295baa
Richard Terry <rterry@internode.on.net>
2011-11-20 21:57:23 +0000

    some queries

---
+++ db/coding/icpc2_drop_tables.sql
@@ -0,0 +1,14 @@
+-- drops all the icpc2 tables
+
+DROP TABLE coding.icpc2_codes;
+DROP TABLE coding.icpc2_components;
+DROP TABLE coding.icpc2_drsdesk_term_mapper;
+DROP TABLE coding.icpc2_grp_grouper CASCADE;
+DROP TABLE coding.icpc2_grp_grp_icpc;
+DROP TABLE coding.icpc2_grp_keyword CASCADE;
+DROP TABLE coding.icpc2_grp_keyword_grp;
+DROP TABLE coding.icpc2_keywords cascade;
+DROP TABLE coding.icpc2_link_keyword_term;
+DROP TABLE coding.icpc2_terms;
+DROP TABLE coding.icpc2_user_terms;
+DROP TABLE coding.icpc2_version;

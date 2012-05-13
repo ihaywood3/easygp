8d1afa202cc6f2ecc8b7ad396f882ff49670e004
Richard Terry <rterry@internode.on.net>
2012-05-13 23:19:05 +0000

    oops.... missed a view in updates

--- db/updates/0.159_update_admin_staff.sql
+++ db/updates/0.159_update_admin_staff.sql
@@ -97,6 +97,18 @@ CREATE OR REPLACE VIEW clerical.vwtaskscomponentsandnotes AS
   ORDER BY vwpatients.fk_patient, task_components.pk;
 
 
+CREATE OR REPLACE VIEW clerical.vwtaskscomponentsnotes AS 
+ SELECT task_component_notes.pk AS pk_note, task_component_notes.fk_task_component, task_component_notes.note, task_component_notes.date, task_component_notes.fk_staff_made_note, vwstaffinclinics.wholename AS staff_made_note_wholename, vwstaffinclinics.title AS staff_made_note_title, task_components.fk_task
+   FROM clerical.task_component_notes
+   JOIN admin.vwstaffinclinics ON task_component_notes.fk_staff_made_note = vwstaffinclinics.fk_staff
+   JOIN clerical.task_components ON task_component_notes.fk_task_component = task_components.pk;
+
+ALTER TABLE clerical.vwtaskscomponentsnotes OWNER TO easygp;
+GRANT ALL ON TABLE clerical.vwtaskscomponentsnotes TO easygp;
+GRANT ALL ON TABLE clerical.vwtaskscomponentsnotes TO staff;
+
+
+
 CREATE OR REPLACE VIEW documents.vwinboxstaff AS 
          SELECT vwstaffinclinics.pk_view, vwstaffinclinics.title, vwstaffinclinics.fk_staff, vwstaffinclinics.wholename, vwstaffinclinics.surname, NULL::unknown AS fk_unmatched_staff
            FROM admin.vwstaffinclinics

9d5b5833118a757e3b77e014ad6cdf82c1a48cda
Richard Terry <rterry@internode.on.net>
2012-07-18 22:31:07 +0000

    pbs update query

---
+++ db/updates/0.196_update_pbs_schedule.sql
@@ -0,0 +1,17 @@
+update drugs.product set fk_schedule=4;
+ -- set them all to 4 to start
+update drugs.product set fk_schedule=8 where atccode ilike 'N01AH%';
+ --opioid anaesthetics
+update drugs.product set fk_schedule=8 where atccode ilike 'N02A%' or atccode ilike 'R05DA%';
+ -- rest of the opioids
+update drugs.product set fk_schedule=8 where atccode ilike 'N06B%';
+ -- Ritalin and friends
+update drugs.product set fk_schedule=8 where atccode ilike 'N01AF%' or
+atccode ilike 'N01AG%' or atccode ilike 'N03AA%' or atccode ilike 'N05CA%';
+ -- barbiturates
+update drugs.product set fk_schedule=8 where atccode = 'N05CD03';
+ -- Rohypnol
+
+truncate db.lu_version;
+insert into db.lu_version (lu_major,lu_minor) values (0, 195);
+

79f939b712667b9cfbb23cf844c81b56fa26b775
Richard Terry <rterry@internode.on.net>
2012-08-26 12:47:08 +0000

    Improved jump to in drug pdfs work on medibase import changed code in DrugsBrowser to add new drugs with ians new pack stuff Prescriptions now works for DVA

---
+++ db/updates/0.221_update_boostrixipv.sql
@@ -0,0 +1,8 @@
+-- Boostrix ipv in the database has a \n character in between the boostrix and the ipv and the tetanus and the toxoid
+update drugs.brand set brand = 'BOOSTRIX IPV' WHERE PK ='e29947a8-0424-4028-b9ac-503e1b79b261';
+update drugs.product set generic = 'pertussis vaccine;diphtheria vaccine;tetanus toxoid;inactivated poliovirus vaccine' where pk = 'be846933-c673-4598-a977-aec691aad909';
+
+truncate db.lu_version;
+insert into db.lu_version (lu_major,lu_minor) values (0, 220);
+
+

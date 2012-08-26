338ce782b56a57226a5f32a593b68fc4777ae92c
Horst Herb <hherb@dorrigomedical.com.au>
2012-08-26 21:53:07 +0000

    db version number update fixed to correct 221

--- db/updates/0.221_update_boostrixipv.sql
+++ db/updates/0.221_update_boostrixipv.sql
@@ -3,6 +3,6 @@ update drugs.brand set brand = 'BOOSTRIX IPV' WHERE PK ='e29947a8-0424-4028-b9ac
 update drugs.product set generic = 'pertussis vaccine;diphtheria vaccine;tetanus toxoid;inactivated poliovirus vaccine' where pk = 'be846933-c673-4598-a977-aec691aad909';
 
 truncate db.lu_version;
-insert into db.lu_version (lu_major,lu_minor) values (0, 220);
+insert into db.lu_version (lu_major,lu_minor) values (0, 221);
 
 

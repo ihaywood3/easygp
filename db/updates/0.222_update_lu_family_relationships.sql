da3e6204d69d9dae52f370c4c4a0114e451fe2fb
Richard Terry <rterry@internode.on.net>
2012-08-28 05:53:31 +0000

    new relationship in family (well, old, I just forgot father)

---
+++ db/updates/0.222_update_lu_family_relationships.sql
@@ -0,0 +1,6 @@
+-- forgot poor old dad!
+insert into common.lu_family_relationships (relationship) values ('father');
+
+truncate db.lu_version;
+insert into db.lu_version (lu_major,lu_minor) values (0, 221);
+

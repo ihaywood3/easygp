79ae7b184d78c3b1cdfe43b40e037fdcf449701f
Richard Terry <rterry@internode.on.net>
2012-08-28 05:54:24 +0000

    fogot the versioning

--- db/updates/0.222_update_lu_family_relationships.sql
+++ db/updates/0.222_update_lu_family_relationships.sql
@@ -2,5 +2,5 @@
 insert into common.lu_family_relationships (relationship) values ('father');
 
 truncate db.lu_version;
-insert into db.lu_version (lu_major,lu_minor) values (0, 221);
+insert into db.lu_version (lu_major,lu_minor) values (0, 222);
 

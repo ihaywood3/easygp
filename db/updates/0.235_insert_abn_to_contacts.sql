c2416df033a88209e9090443c515afcf73411bff
Richard Terry <rterry@internode.on.net>
2012-10-15 21:12:34 +0000

    fixed error in script

--- db/updates/0.235_insert_abn_to_contacts.sql
+++ db/updates/0.235_insert_abn_to_contacts.sql
@@ -1,4 +1,4 @@
-alter table  data_numbers add column australian_business_number text default null;
+alter table contacts.data_numbers add column australian_business_number text default null;
 
 truncate db.lu_version;
 insert into db.lu_version (lu_major,lu_minor) values (0, 235);

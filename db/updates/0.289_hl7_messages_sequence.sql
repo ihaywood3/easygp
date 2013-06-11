65c41ee1a40d97f8213ac12dcdc3132cfae30e6e
Richard Terry <rterry@internode.on.net>
2013-06-11 00:56:14 +0000

    swapped back the sequence name to hl7_messages as Ian says the latest svn code uses that

--- db/updates/0.289_hl7_messages_sequence.sql
+++ db/updates/0.289_hl7_messages_sequence.sql
@@ -1,9 +1,9 @@
 -- create a sequence for generating HL7 message numbers
 
-create sequence documents.hl7_messages_seq;
-ALTER sequence documents.hl7_messages_seq OWNER TO easygp;
-GRANT ALL ON TABLE documents.hl7_messages_seq TO easygp;
-GRANT ALL ON TABLE documents.hl7_messages_seq TO staff;
+create sequence documents.hl7_messages;
+ALTER sequence documents.hl7_messages OWNER TO easygp;
+GRANT ALL ON TABLE documents.hl7_messages TO easygp;
+GRANT ALL ON TABLE documents.hl7_messages TO staff;
 
 truncate db.lu_version;
 insert into db.lu_version (lu_major,lu_minor) values (0, 289);

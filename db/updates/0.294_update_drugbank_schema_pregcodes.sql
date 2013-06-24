8fd73a3e1a94697ae97568dbbfef5124b2c3d472
Horst Herb <hherb@dorrigomedical.com.au>
2013-06-24 13:06:20 +0000

    Adding pregnancy codes table

---
+++ db/updates/0.294_update_drugbank_schema_pregcodes.sql
@@ -0,0 +1,17 @@
+
+CREATE TABLE drugbank.pregnancy_code
+(
+  fk_drug integer,
+  code character(1),
+  safety text,
+  pk integer NOT NULL DEFAULT nextval('drugbank.pregnancycode_pk_seq'::regclass),
+  CONSTRAINT pregnancy_code_pkey PRIMARY KEY (pk),
+  CONSTRAINT pregnancy_code_fk_drug_fkey FOREIGN KEY (fk_drug)
+      REFERENCES drugbank.drug (pk) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE drugbank.pregnancy_code
+  OWNER TO easygp;

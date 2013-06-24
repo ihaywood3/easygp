a6865efcbc8e717e563feb750670489c9a06022a
Horst Herb <hherb@dorrigomedical.com.au>
2013-06-24 13:34:28 +0000

    Ooops, pregnancy codes can be 2 characters long of course

--- db/updates/0.294_update_drugbank_schema_pregcodes.sql
+++ db/updates/0.294_update_drugbank_schema_pregcodes.sql
@@ -2,7 +2,7 @@
 CREATE TABLE drugbank.pregnancy_code
 (
   fk_drug integer,
-  code character(1),
+  code character(2),
   safety text,
   pk integer NOT NULL DEFAULT nextval('drugbank.pregnancycode_pk_seq'::regclass),
   CONSTRAINT pregnancy_code_pkey PRIMARY KEY (pk),

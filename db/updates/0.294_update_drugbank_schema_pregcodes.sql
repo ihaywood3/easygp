64bb17dfc3c6c83b34818eabe6172898a2360056
Horst Herb <hherb@dorrigomedical.com.au>
2013-06-25 12:17:23 +0000

    Sorry, I had messed up the table definition and forgot to update the database version. Won't happen again - Apologies. This script will drop the table if it exists already and then create it

--- db/updates/0.294_update_drugbank_schema_pregcodes.sql
+++ db/updates/0.294_update_drugbank_schema_pregcodes.sql
@@ -1,11 +1,10 @@
-
+DROP TABLE drugbank.pregnancy_code;
 CREATE TABLE drugbank.pregnancy_code
 (
+  pk SERIAL PRIMARY KEY, 
   fk_drug integer,
   code character(2),
   safety text,
-  pk integer NOT NULL DEFAULT nextval('drugbank.pregnancycode_pk_seq'::regclass),
-  CONSTRAINT pregnancy_code_pkey PRIMARY KEY (pk),
   CONSTRAINT pregnancy_code_fk_drug_fkey FOREIGN KEY (fk_drug)
       REFERENCES drugbank.drug (pk) MATCH SIMPLE
       ON UPDATE NO ACTION ON DELETE NO ACTION
@@ -13,5 +12,11 @@ CREATE TABLE drugbank.pregnancy_code
 WITH (
   OIDS=FALSE
 );
-ALTER TABLE drugbank.pregnancy_code
-  OWNER TO easygp;
+
+COMMENT ON COLUMN drugbank.pregnancy_code.code IS 'ABCD pregnancy category as per Australian TGA definition';
+COMMENT ON COLUMN drugbank.pregnancy_code.safety IS 'comment on safety of the referenced drug in pregnancy'; 
+
+ALTER TABLE drugbank.pregnancy_code OWNER TO easygp;
+  
+truncate db.lu_version;
+insert into db.lu_version (lu_major,lu_minor) values (0, 294);

b1515063a5ed52f92c4b676e10b3b980cb23027f
Horst Herb <hherb@dorrigomedical.com.au>
2013-09-28 04:03:27 +0000

    view for basic drug pi (geneic) including pregnancy code

---
+++ db/updates/0.324_update_drugbank_views.sql
@@ -0,0 +1,11 @@
+-- View: drugbank.vw_basic_drug_pi
+
+-- DROP VIEW drugbank.vw_basic_drug_pi;
+
+CREATE OR REPLACE VIEW drugbank.vw_basic_drug_pi AS 
+ SELECT pregnancy_code.code AS pregnancy_code, pregnancy_code.safety AS pregnancy_safety, drug.pk, drug.drugbank_id, drug.description, drug.cas_number, drug.indication, drug.pharmacology, drug.mechanism_of_action, drug.toxicity, drug.biotransformation, drug.absorption, drug.half_life, drug.route_of_elimination, drug.volume_of_distribution, drug.clearance, drug.name
+   FROM drugbank.drug
+   LEFT JOIN drugbank.pregnancy_code ON pregnancy_code.fk_drug = drug.pk;
+
+ALTER TABLE drugbank.vw_basic_drug_pi
+  OWNER TO easygp;

f618815a2d3d7c745778e87da470dcdfa3c59530
Richard Terry <rterry@internode.on.net>
2013-10-02 22:02:24 +0000

    Renamed the progressnotes viewer file and replaced all instances updated .project to relect the latest permissions fix

--- db/updates/0.327_fix_permissions_yet_again_aaarrrrgh.sql
+++ db/updates/0.327_fix_permissions_yet_again_aaarrrrgh.sql
@@ -5,3 +5,5 @@ grant usage on chronic_disease_management.epc_referral_pk_seq to staff;
 grant usage on chronic_disease_management.lu_allied_health_type_pk_seq to staff;
 grant usage on chronic_disease_management.lu_dacc_components_pk_seq to staff;
 grant usage on chronic_disease_management.team_care_arrangements_pk_seq to staff;
+truncate db.lu_version;
+insert into db.lu_version (lu_major,lu_minor) values (0, 327)

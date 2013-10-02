b4c6d4dcfd31a6825ba793cc9b3297bbaa46d6c0
Horst Herb <hherb@dorrigomedical.com.au>
2013-10-02 02:08:39 +0000

    diabetes cycle of care crahses easygp because of missing permissions for staff. Again! Aaaargh!!!

---
+++ db/updates/0.327_fix_permissions_yet_again_aaarrrrgh.sql
@@ -0,0 +1,7 @@
+grant usage on chronic_disease_management.diabetes_annual_cycle_of_care_notes_pk_seq to staff;
+grant usage on chronic_disease_management.diabetes_annual_cycle_of_care_pk_seq to staff;
+grant usage on chronic_disease_management.epc_link_provider_form_pk_seq to staff;
+grant usage on chronic_disease_management.epc_referral_pk_seq to staff;
+grant usage on chronic_disease_management.lu_allied_health_type_pk_seq to staff;
+grant usage on chronic_disease_management.lu_dacc_components_pk_seq to staff;
+grant usage on chronic_disease_management.team_care_arrangements_pk_seq to staff;

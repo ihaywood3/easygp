7f3fb076ba17de3349c041e99241c006752dc110
Richard Terry <rterry@internode.on.net>
2014-02-02 10:58:46 +0000

    Minor modifications to the health summary
    FRequestsNew - duplicate form whilst I implement horst's wish list to change functionality of searching and accepting terms
                 - hopefully removed a really annoying long standing bug occurring when editing/resaving an existing request
                   I'm surprised no-one has noticed this as it caused a duplication of the form and annoyed the crap out of me.
    
    FProgressNoteEditor 	- added some buttons to make it easier to quickly code using terms you use frequently (hidden for the moment)
                            - Changed the way templates save ... you can now save an existing template
    
    FReferrals 		- Added a simple combo and code to save a referral letter template as per Horsts wishes.
    
    
    FSaveNotesTEmplate	- changed to use cRow to save/edit template
    
    Next Commit I will swap over the FRequestsNew after I delete the old FRequests.
    
    modParseDocument	- Improved pathology parsing
    
    0.355_create_letter_templates.sql  - table to support templates in referral letters module.
    
    Please report any bugs.

---
+++ db/updates/0.355_create_letter_templates.sql
@@ -0,0 +1,29 @@
+
+-- new table to enable user to save templates in the letter writer
+
+CREATE TABLE clin_referrals.lu_referral_letter_templates
+(
+  pk serial NOT NULL,
+  fk_staff integer NOT NULL,
+  shared boolean DEFAULT false, -- if true then anyone can access this template
+  name text NOT NULL,
+  deleted boolean DEFAULT false,
+  template text NOT NULL, -- html for a letter template
+ 
+ CONSTRAINT lu_referral_letter_templates_fk_staff_fkey FOREIGN KEY (fk_staff)
+      REFERENCES admin.staff (pk) MATCH SIMPLE
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+       CONSTRAINT lu_referral_letter_templates_pkey PRIMARY KEY (pk )
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE clin_referrals.lu_referral_letter_templates   OWNER TO easygp;
+GRANT ALL ON TABLE clin_referrals.lu_referral_letter_templates TO staff;
+
+COMMENT ON TABLE clin_referrals.lu_referral_letter_templates   IS 'Table to hold templates for referral letters';
+COMMENT ON COLUMN clin_referrals.lu_referral_letter_templates.shared IS 'if true then anyone can access this template';
+COMMENT ON COLUMN clin_referrals.lu_referral_letter_templates.template IS 'html for a letter template';
+
+update db.lu_version set lu_minor=354;
+

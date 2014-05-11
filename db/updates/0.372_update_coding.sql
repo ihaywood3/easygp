c715b5fb2a0e9d81af41c313b2770d9dfe0f06c3
Richard Terry <rterry@internode.on.net>
2014-05-11 11:19:37 +0000

    fixed another typo

--- db/updates/0.372_update_coding.sql
+++ db/updates/0.372_update_coding.sql
@@ -1,5 +1,5 @@
 GRANT ALL ON table coding.user_favourites to STAFF;
-GRANT  USAGE ON table coding.user_favourites_pk_seq to STAFF:
+GRANT  USAGE ON table coding.user_favourites_pk_seq to STAFF;
 
 update db.lu_version set lu_minor=372;
 

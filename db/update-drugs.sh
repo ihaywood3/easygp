3fae9bbdf5daefe47b09fb4f5eeb27db66d65cf3
Ian Haywood <ian@haywood.id.au>
2014-05-24 23:46:46 +0000

    update drugs DB via cron

---
+++ db/update-drugs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /var/lib/easygp/drug_pis
+
+rsync -tqz haywood.id.au::drugs/*.sql .
+if [ drugs.sql -nt drugs-stamp ] ; then
+    # don't sweat the DB unless the drugs has actually changed
+    touch drugs-stamp
+    psql -f drugs.sql -U easygp easygp
+fi

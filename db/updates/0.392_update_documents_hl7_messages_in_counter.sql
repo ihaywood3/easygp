f557ddd0694cf109098a2167bab2f4a86ecaceed
Richard Terry <rterry@internode.on.net>
2014-11-13 03:26:37 +0000

    - implemented unique file naming of all incoming hl7 files prior to being parsed - general bugfixes

---
+++ db/updates/0.392_update_documents_hl7_messages_in_counter.sql
@@ -0,0 +1,16 @@
+﻿
+CREATE SEQUENCE documents.hl7_messages_in
+  INCREMENT 1
+  MINVALUE 1
+  MAXVALUE 9223372036854775807
+  START 1
+  CACHE 1;
+ALTER TABLE documents.hl7_messages_in   OWNER TO easygp;
+GRANT ALL ON TABLE documents.hl7_messages_in TO easygp;
+GRANT ALL ON TABLE documents.hl7_messages_in TO staff;
+
+comment on SEQUENCE documents.hl7_messages_in is
+'a unique key which is added to all incoming hl7 files imported to prevent collisions
+ due to different senders using the same file names';
+ 
+update db.lu_version set lu_minor=392;

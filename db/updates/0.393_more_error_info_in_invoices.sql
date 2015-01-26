5c6aa438d2019eebc4682a35ea2bba5c58ec459b
Ian Haywood <ian@haywood.id.au>
2015-01-26 01:40:58 +0000

    more error codes

--- db/updates/0.393_more_error_info_in_invoices.sql
+++ db/updates/0.393_more_error_info_in_invoices.sql
@@ -80,7 +80,8 @@ create or replace view billing.vwinvoices as
     invoices.result_text,
     invoices.error_level,
     invoices.pms_claim_id,
-    codes.description
+    c1.description,
+    c2.description as claim_description
    FROM billing.invoices
    JOIN admin.vwstaff staff_invoicing ON invoices.fk_staff_invoicing = staff_invoicing.fk_staff
    JOIN admin.vwstaff staff_provider ON invoices.fk_staff_provided_service = staff_provider.fk_staff
@@ -90,7 +91,8 @@ create or replace view billing.vwinvoices as
    LEFT JOIN contacts.vwpersonsincludingpatients ON invoices.fk_payer_person = vwpersonsincludingpatients.fk_person
    LEFT JOIN contacts.vwpatients ON invoices.fk_patient = vwpatients.fk_patient
    LEFT JOIN billing.claims ON invoices.fk_claim = claims.pk
-   LEFT JOIN billing.codes ON invoices.result_code = codes.code;
+   LEFT JOIN billing.codes c1 ON invoices.result_code = c1.code
+   LEFT JOIN billing.codes c2 ON claims.result_code = c2.code;
 
 create or replace view billing.vwitemsbilled as 
  SELECT items_billed.pk AS pk_items_billed,
@@ -103,6 +105,7 @@ create or replace view billing.vwitemsbilled as
     fee_schedule.mbs_item,
     fee_schedule.user_item,
     fee_schedule.ama_item,
+    coalesce(mbs_item,ama_item,user_item) as common_item,
     fee_schedule.descriptor,
     fee_schedule.descriptor_brief,
     fee_schedule.gst_rate,
@@ -117,9 +120,91 @@ create or replace view billing.vwitemsbilled as
     JOIN billing.fee_schedule ON items_billed.fk_fee_schedule = fee_schedule.pk
     LEFT JOIN billing.codes ON items_billed.reason_code = codes.code;
 
+
+CREATE or replace VIEW billing.vwitemsandinvoices AS
+ SELECT vwitemsbilled.pk_items_billed,
+    vwitemsbilled.fk_fee_schedule,
+    vwitemsbilled.amount,
+    vwitemsbilled.amount_gst,
+    vwitemsbilled.fk_lu_billing_type,
+    vwitemsbilled.billing_type,
+    vwitemsbilled.mbs_item,
+    vwitemsbilled.user_item,
+    vwitemsbilled.ama_item,
+    vwitemsbilled.descriptor,
+    vwitemsbilled.descriptor_brief,
+    vwitemsbilled.gst_rate,
+    vwitemsbilled.percentage_fee_rule,
+    vwinvoices.fk_invoice,
+    vwinvoices.notes,
+    vwinvoices.fk_staff_invoicing,
+    vwinvoices.fk_patient,
+    vwinvoices.date_printed,
+    vwinvoices.fk_staff_provided_service,
+    vwinvoices.date_invoiced,
+    vwinvoices.paid,
+    vwinvoices.fk_payer_person,
+    vwinvoices.fk_payer_branch,
+    vwinvoices.account_to_name,
+    vwinvoices.account_to_branch,
+    vwinvoices.account_to_street,
+    vwinvoices.account_to_town_postcode,
+    vwinvoices.latex,
+    vwinvoices.fk_branch,
+    vwinvoices.visit_date,
+    vwinvoices.fk_appointment,
+    vwinvoices.appointment_time,
+    vwinvoices.duration,
+    vwinvoices.reference,
+    vwinvoices.fk_lu_bulk_billing_type,
+    vwinvoices.total_bill,
+    vwinvoices.total_paid,
+    vwinvoices.total_gst,
+    vwinvoices.due,
+    vwinvoices.staff_invoicing_wholename,
+    vwinvoices.staff_provided_service_wholename,
+    vwinvoices.staff_provided_service_provider_number,
+    vwinvoices.australian_business_number,
+    vwinvoices.patient_firstname,
+    vwinvoices.patient_surname,
+    vwinvoices.patient_title,
+    vwinvoices.patient_fk_sex,
+    vwinvoices.patient_sex,
+    vwinvoices.patient_wholename,
+    vwinvoices.fk_lu_centrelink_card_type,
+    vwinvoices.fk_lu_default_billing_level,
+    vwinvoices.branch,
+    vwinvoices.result_code,
+    vwinvoices.result_text,
+    vwitemsbilled.reason_code,
+    vwitemsbilled.comment AS item_comment,
+    vwitemsbilled.service_id,
+    vwinvoices.voucher_id,
+    vwinvoices.fk_claim,
+    vwinvoices.pms_claim_id,
+    vwinvoices.error_level as error_level,
+    vwinvoices.description as description,
+    vwitemsbilled.error_level as item_error_level,
+    vwitemsbilled.description as item_description,
+    vwinvoices.claim_description,
+    vwinvoices.claim_result_code,
+    vwinvoices.claim_result_text,
+    vwinvoices.online,
+    vwitemsbilled.common_item,
+    vwinvoices.claim_id,
+    vwinvoices.claim_date
+   FROM billing.vwitemsbilled,
+    billing.vwinvoices
+  WHERE (vwinvoices.fk_invoice = vwitemsbilled.fk_invoice);
+
+
+
+
+
 truncate billing.codes;
 
 COPY billing.codes (code, description) FROM stdin;
+0    Success
 1001	Unable to load /connect to Java Virtual Machine.
 1002	Unable to unload Medicare Online Claiming.
 1003	Medicare Online Claiming is not operational.
@@ -884,6 +969,13 @@ COPY billing.codes (code, description) FROM stdin;
 4003	Bulk-billing online waiting to be assigned to a claim
 4004	Bulk-billing assigned to claim
 4005	Private billing awaiting transmission
+4006	No invoice-level error code: see item status
+4007	No invoice-level error code: see claim status
+4008	Claim awaiting transmission
+4009	Python layer exception on bulk-billing
+4010	Bulk-bill claim transmitted no result yet
+4011	Error code 9501 but no report provided
+4012	Python layer exception on private billing
 101	More details of service required to assess benefit                      
 102	No amount charged is shown on account/receipt                           
 103	Letter of explanation is being sent separately                          

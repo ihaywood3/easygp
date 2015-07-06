d5df2213d1eb692ce73c506a605f3ee7bf74f8d6
Richard Terry <rterry@internode.on.net>
2015-07-06 04:25:53 +0000

    - a previous import hl7 error had meant that loinc code dropped where the data was not straight numeric - fixed - BugFix in FDACC where the data of next eGFR due was not correctly calculated - updated pathology parsing

---
+++ db/updates/0.402_update_egfr_missing_loincs.sql
@@ -0,0 +1,3 @@
+﻿ -- bug in importing HL7 meant no loinc code put into egfr where value >maximum range value
+update documents.observations set loinc='33914-3' where identifier ilike 'egfr' and loinc is null;
+update db.lu_version set lu_minor=402;

1974989b582520a042d1a9b501db232d89cc5d94
Richard Terry <rterry@internode.on.net>
2015-08-27 13:11:39 +0000

    0.405_update_consult.lu_progressnote_templates.sql      adds new columns to allow user to code a template and to auto-code a consult when the template is selected
    0.406_documents.unique_file_number_seq.sql              adds a new sequence to generate a unique file number for use with temporary files etc JOINPDF (see below)
    
    modParseDocument                                        improved pathology parsing
    
    modProgressNotes                                        the collection of templates for the Notes Editor Combo box is now returned ordered by name of template
    
    modArchiveDocumentsDBI			New             Query:Unique_filenum_For_Joining() pulls a unique filenumber from a new backend table in documents (see 0.406 update query)
    
    FArchiveDocuments                       BuFix           JoinPDF had a rather fatal bug. If two persons filing on different workstations there could be a clash
                                                            of temporary file names leading to EasyGP dying and any pdf just joined not being created and the 
                                                            underlying pdf's attempting to be joined would also be lost. (!) (Fixed)
                                                         
    FDrugMaintenance                                        Improved gui functionality around the generics grid There are still lots of gui-bugs in this grid and bad behaviour
    	                                                Better validation on generics selection, should no longer be possible to select duplicate generics into the grid
    	                                                
                                                         
    FMedicalCertificates 			GuiFix		popup coded terms selector would be visible when txtCodedTerm got focus
                                                            even when TxtCodedTerm.text was "" (fixed)
                                                            
    FCodedTermSelector		                        Removed the timer, cleaned up the code
    
    FProgressNoteEditor                     New              Changes to the way templates combo functions
                                                            - they are now added to the combo in alphabetical order by name
                                                            - the popup editor menu option for 'Save Template' now includes the name of any existing template loaded as a menu item
                                                            - the popup editor menu option for Save Existiing is now labelled 'Edit Template' followed by the template name
                                                            - there is a new popup menu option 'Delete Template' followed by the template name.
     
    FSaveNotesTemplate                      BugFix           When an existing template is edited the sharing data is now shown appropriately
      				        New              You can now optionally code a template entry and tell easyGP to use the code to auto-code your consultation
      				                         By editing an existing template you can of course re-name or change its name (good also for old typo's)
      				                         
    FReferrals	                                         Changed popup menu titles to reflect name of existing template, can now delete a template

---
+++ db/updates/0.405_update_consult.lu_progressnote_templates.sql
@@ -0,0 +1,29 @@
+﻿alter table clin_consult.lu_progressnote_templates add column code text default null;
+COMMENT ON COLUMN clin_consult.lu_progressnote_templates.code is
+	'if not null, key to coding.generic_terms table. This code can be used to auto-code consultations where a template is used
+	 for example annual checkup > check up;adult health complete';
+	 
+alter table clin_consult.lu_progressnote_templates add column auto_code_consult boolean default false;
+COMMENT ON COLUMN clin_consult.lu_progressnote_templates.auto_code_consult is 
+	'if True then when a template is chosen the consultation will be auto coded if a template is coded (has an fk_code)';
+	
+Create or replace view clin_consult.vwProgressNotesTemplates as
+SELECT 
+  lu_progressnote_templates.pk, 
+  lu_progressnote_templates.fk_staff, 
+  lu_progressnote_templates.shared, 
+  lu_progressnote_templates.name, 
+  lu_progressnote_templates.deleted, 
+  lu_progressnote_templates.template, 
+  lu_progressnote_templates.code, 
+  lu_progressnote_templates.auto_code_consult, 
+  generic_terms.term,
+  generic_terms.fk_coding_system
+  FROM clin_consult.lu_progressnote_templates
+  LEFT JOIN   coding.generic_terms ON  lu_progressnote_templates.code = generic_terms.code
+  ;
+  
+  ALTER TABLE clin_consult.vwProgressNotesTemplates OWNER TO easygp;
+  GRANT SELECT ON clin_consult.vwProgressNotesTemplates to staff;
+
+  update db.lu_version set lu_minor=405;
---
+++ db/updates/0.406_documents.unique_file_number_seq.sql
@@ -0,0 +1,10 @@
+﻿CREATE SEQUENCE documents.unique_file_number_seq
+   INCREMENT 1
+   MINVALUE 1
+   MAXVALUE 9223372036854775807
+   START 1
+   CACHE 1;
+ALTER TABLE documents.unique_file_number_seq  OWNER TO EASYGP;
+GRANT USAGE ON documents.unique_file_number_seq TO Staff; 
+
+update db.lu_version set lu_minor=406;

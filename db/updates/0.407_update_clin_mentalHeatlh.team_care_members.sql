ed064f9223ea4e02a34c834cfafa785c1ef29907
Richard Terry <rterry@internode.on.net>
2015-09-08 22:05:56 +0000

    Work on Mental Health bugs (not critical) and Guifixes
    0.408_update_clin_mental_health.team_care_members.sql 	fixes backend table entries with fk_organisation, fk_branch and fk_employee = 0 where fk_person is non zero
    
    modMentalHealthDBI		        -- minor syntax change in a query pulling in plans (forgot a space between number and order by statement
                                               this didn't cause a crash but looked odd in the log)
    
    FMentalHealth		 BugFix		- If a plan is re-edited and the psychologist was a person as sole trader (not employee in organisation)
                                              then their name was not reloaded into the cvwTeamCareProvders and seemed lost                 (fixed)
                                            - when an existing psychologist was deleted from team care list, the row was not reset to the next provider
                                              the gui still showed the deleted providers task (mis-leading) 				(fixed)
                             BugFix         - Patient as a team care member (they are auto-included with each new plan) coudl be deleted 	(fixed)
                                              
    FMedicalCertificates     GuiFix	        - If user clicked on the calandar button, then changed mind > typed in date textbox then
                                              popup calander didn't disappear.	                                                        (fixed)(fixed)	
                                              Also if user now hits <ESC> key and calandar is showing it is now removed.

---
+++ db/updates/0.407_update_clin_mentalHeatlh.team_care_members.sql
@@ -0,0 +1,11 @@
+﻿-- bugfix where these fields were inadvertantly being set to 0 by the gambas code
+-- this meant that on editing, persons where were not patients were not showing up in the gui.
+ALTER TABLE clin_mentalhealth.team_care_members alter column fk_organisation set default null;
+ALTER TABLE clin_mentalhealth.team_care_members alter column fk_branch set default null;
+ALTER TABLE clin_mentalhealth.team_care_members alter column fk_employee set default null;
+
+update clin_mentalhealth.team_care_members  set fk_organisation=null where fk_organisation=0;
+update clin_mentalhealth.team_care_members  set fk_branch=null where fk_branch=0;
+update clin_mentalhealth.team_care_members  set fk_employee=null where fk_employee=0;
+
+update db.lu_version set lu_minor=407;

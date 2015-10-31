936484839bf34977b5be251736184c09313e9342
Richard Terry <rterry@internode.on.net>
2015-10-31 13:57:56 +0000

    more bugs

--- db/updates/0.429_drop_unwanted_tables.sql
+++ db/updates/0.429_drop_unwanted_tables.sql
@@ -2,5 +2,5 @@
 drop table chronic_disease_management.lu_dacc_components ;
 drop table chronic_disease_management.team_care_arrangements;
 DROP TABLE chronic_disease_management.epc_link_provider_form;
-
+DROP TABLE chronic_disease_management.epc_referral;
 update db.lu_version set lu_minor=429;

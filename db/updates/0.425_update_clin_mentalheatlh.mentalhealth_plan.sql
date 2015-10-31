b8419f535a6dc71459fb66864d631a595c7cc1d2
Richard Terry <rterry@internode.on.net>
2015-10-31 05:04:30 +0000

    -- dropped an unused column

---
+++ db/updates/0.425_update_clin_mentalheatlh.mentalhealth_plan.sql
@@ -0,0 +1,49 @@
+﻿-- dropped a column not currently used
+
+alter table clin_mentalhealth.mentalhealth_plan drop column fk_stress_assessment cascade;
+-- DROP VIEW clin_mentalhealth.vwmentalhealthplans;
+
+CREATE OR REPLACE VIEW clin_mentalhealth.vwmentalhealthplans AS 
+ SELECT mentalhealth_plan.pk AS pk_mentalhealth_plan,
+    consult.fk_patient,
+    consult.fk_staff,
+    consult.consult_date AS plan_date,
+    mentalhealth_plan.fk_consult,
+    mentalhealth_plan.fk_pasthistory,
+    mentalhealth_plan.diagnosis,
+    mentalhealth_plan.fk_coding_system,
+    mentalhealth_plan.fk_progressnote,
+    lu_systems.system,
+    ( SELECT DISTINCT generic_terms.term
+           FROM coding.generic_terms
+          WHERE mentalhealth_plan.fk_code = generic_terms.code) AS coded_term,
+    mentalhealth_plan.fk_code,
+    mentalhealth_plan.presenting_problems,
+    mentalhealth_plan.bio_psycho_social,
+    mentalhealth_plan.mental_state_examination,
+    mentalhealth_plan.fk_lu_risk_to_others,
+    lu_risk_to_others.risk,
+    mentalhealth_plan.relapse_plan,
+    mentalhealth_plan.risk_harm_comments,
+    mentalhealth_plan.goals,
+    mentalhealth_plan.treatment_referrrals,
+    mentalhealth_plan.review_date,
+    mentalhealth_plan.html,
+    mentalhealth_plan.fk_lu_plan_type,
+    lu_plan_type.type,
+    mentalhealth_plan.deleted,
+    vwstaff.wholename,
+    vwstaff.title
+   FROM clin_mentalhealth.mentalhealth_plan
+     JOIN clin_consult.consult ON mentalhealth_plan.fk_consult = consult.pk
+     JOIN clin_mentalhealth.lu_plan_type ON mentalhealth_plan.fk_lu_plan_type = lu_plan_type.pk
+     LEFT JOIN clin_history.past_history ON mentalhealth_plan.fk_pasthistory = past_history.pk
+     JOIN coding.lu_systems ON mentalhealth_plan.fk_coding_system = lu_systems.pk
+     LEFT JOIN clin_mentalhealth.lu_risk_to_others ON mentalhealth_plan.fk_lu_risk_to_others = lu_risk_to_others.pk
+     JOIN admin.vwstaff ON consult.fk_staff = vwstaff.fk_staff
+  WHERE NOT mentalhealth_plan.deleted;
+
+ALTER TABLE clin_mentalhealth.vwmentalhealthplans   OWNER TO easygp;
+GRANT SELECT ON TABLE clin_mentalhealth.vwmentalhealthplans TO staff;
+
+update db.lu_version set lu_minor=425; 

49f3f3767c1a8321c92f848f303bb12aaea25d72
Richard Terry <rterry@internode.on.net>
2015-10-31 13:45:16 +0000

    another bug in the script

--- db/updates/0.431_add_missing_request.1507.sql
+++ db/updates/0.431_add_missing_request.1507.sql
@@ -1,7 +1,7 @@
 ﻿INSERT INTO clin_requests.lu_requests(
             pk, fk_lu_request_type, item, fk_laterality, fk_decision_support, 
             fk_instruction, deleted)
-    VALUES (1507, 1, 'Test Not Translated', 0,0,0 ,True);
+    VALUES (1507, 1, 'Test Not Translated', null,null,null ,True);
     
     update db.lu_version set lu_minor=431;
     

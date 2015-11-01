133939731462f47f4f301153579340c6a504c1d2
Richard Terry <rterry@internode.on.net>
2015-11-01 02:29:28 +0000

    more script fixes, fix another request

--- db/updates/0.431_add_missing_request.1507.sql
+++ db/updates/0.431_add_missing_request.1507.sql
@@ -1,6 +1,4 @@
-﻿INSERT INTO clin_requests.lu_requests(
-            pk, fk_lu_request_type, item, fk_laterality, fk_decision_support, 
-            fk_instruction, deleted)
+﻿INSERT INTO clin_requests.lu_requests(pk, fk_lu_request_type, item, fk_laterality, fk_decision_support,fk_instruction, deleted)
     VALUES (1507, 1, 'Test Not Translated', null,null,null ,True);
     
     update db.lu_version set lu_minor=431;
---
+++ db/updates/0.438_add_mssing_request.1508.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO clin_requests.lu_requests(pk, fk_lu_request_type, item, fk_laterality,deleted)
+  VALUES (1508, 1, 'Test Not Translated', null,True);
+
+  update db.lu_version set lu_minor=438;
+    

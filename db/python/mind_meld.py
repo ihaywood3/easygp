713378cfaef41615b2bb5dc5ab8d7e1b06838c0b
Ian Haywood <ian@haywood.id.au>
2015-11-01 04:20:07 +0000

    fixes for use at Dorrigo

--- db/python/mind_meld.py
+++ db/python/mind_meld.py
@@ -26,20 +26,37 @@ import pdb
 db = dbwrapper.DBWrapper({'database':'easygp2','db_user':'ian','host':'','port':None,'password':None},True)
 others= {'fk_consult':'clin_consult.consult','fk_patient':'clerical.data_patients','fk_employee':'contacts.data_employees','fk_person':'contacts.data_persons','fk_staff':'admin.staff','fk_branch':'contacts.data_branches','fk_address':'contacts.data_addresses','fk_progressnote':'clin_consult.progressnotes','fk_pasthistory':'clin_history.past_history','fk_clinic':'admin.clinics','fk_occupation':'common.lu_occupations','fk_generic_product':'drugs.product','fk_document':'documents.documents','fk_coding_system':'coding.lu_systems'}
 
+def convert_all_uuid():
+    drop_all_views()
+    for t in db.each_table(): convert_table_uuid(t)
+    print "ALTER TABLE clin_consult.progressnotes ALTER COLUMN fk_row SET DATA TYPE uuid USING uuid_generate_v5(uuid_ns_url(),'http://easygp.net.au/uuid_conversion/' || linked_table || '/' || fk_row);"
+
+def drop_all_views():
+    for v in db.each_table('.*vw.*','v'):
+        print "DROP VIEW %s CASCADE;" % v
+
 def convert_table_uuid(tbl):
     constraints = db.each_foreign_constraint()
-    if db.column_type(tbl,"pk") == "uuid": return  # no need to do it
+    pk_typ =  db.column_type(tbl,"pk")
+    if pk_typ == "uuid": return  # no need to do it
+    if pk_typ is None: return # can't do it.
     just_tbl = tbl.split(".")[1]   # drop schema
-    for c in constraints:
-        if c["table2"] == tbl and c['col2'] == 'pk':
-            print "ALTER TABLE %s DROP CONSTRAINT %s;" % (c["table1"],c['name'])
-            print "ALTER TABLE %(table1)s ALTER COLUMN %(col1)s SET DATA TYPE uuid USING uuid_generate_v5(uuid_ns_url(),'http://easygp.net.au/uuid_conversion/%(table2)s/' || %(col1)s);" % c
-    print "ALTER TABLE %s DROP CONSTRAINT %s_pkey;" % (tbl,just_tbl)
+    if just_tbl=="shortcuts_user": return # don't do this table
+    if just_tbl.startswith("lu_"): return # don't do lookup tables
+    for table1,x in constraints.items():
+        for col1,c in x.items():
+            if c['table'] == tbl and table1 != 'clin_consult.shortcuts_user':
+                print "ALTER TABLE %s DROP CONSTRAINT %s;" % (table1,c['constraint'])
+                print "ALTER TABLE %s ALTER COLUMN %s SET DATA TYPE uuid USING uuid_generate_v5(uuid_ns_url(),'http://easygp.net.au/uuid_conversion/%s/' || %s);" % (table1,col1,tbl,col1)
+    #print "ALTER TABLE %s DROP CONSTRAINT %s_pkey;" % (tbl,just_tbl)
+    print "ALTER TABLE %s ALTER COLUMN pk DROP DEFAULT;" % tbl
     print "ALTER TABLE %s ALTER COLUMN pk SET DATA TYPE uuid USING uuid_generate_v5(uuid_ns_url(),'http://easygp.net.au/uuid_conversion/%s/' || pk);" % (tbl,tbl)
-    print "ALTER TABLE %s ADD PRIMARY KEY (pk);" % tbl
-    for c in constraints:
-        if c["table2"] == tbl and c['col2'] == 'pk':
-            print "ALTER TABLE %(table1)s ADD FOREIGN KEY (%(col1)s) REFERENCES %(table2)s (pk);" % c
+    print "ALTER TABLE %s ALTER COLUMN pk SET DEFAULT uuid_generate_v4();" % tbl
+    #print "ALTER TABLE %s ADD PRIMARY KEY (pk);" % tbl
+    for table1,x in constraints.items():
+        for col1,c in x.items():
+            if c['table'] == tbl and table1 != 'clin_consult.shortcuts_user':
+                print "ALTER TABLE %s ADD CONSTRAINT %s FOREIGN KEY (%s) REFERENCES %s (pk);" % (table1,c['constraint'],col1,tbl)
 
 
 def add_constraints():
@@ -56,10 +73,11 @@ def add_constraints():
                         if c.startswith(c2):
                             pk_name = 'pk'
                             if c2 == 'fk_code': pk_name = 'code'
-                            print 'ALTER TABLE %s add constraint "%s_%s_fkey" foreign key (%s) references %s (%s);' % (t,t2,c,c,others[c2],pk_name)
+                            print '--ALTER TABLE %s add constraint "%s_%s_fkey" foreign key (%s) references %s (%s);' % (t,t2,c,c,others[c2],pk_name)
                             found = True
-                    if not found:
-                        print '--alter table %s add constraint "%s_%s_fkey" foreign key (%s) references ?? (pk);' % (t,t2,c,c)
+                    if not found and c != 'fk_code':
+                        print 'ALTER TABLE %s add constraint "%s_%s_fkey" foreign key (%s) references  (pk);' % (t,t2,c,c)
 
 add_constraints()
-#convert_table_uuid("clerical.data_patients")
+#convert_all_uuid()
+#drop_all_views()

c36cdbf5c8a944962be932d47d65f8faeac9bfa0
Richard Terry <rterry@internode.on.net>
2015-11-01 03:25:25 +0000

    more bugs

--- db/updates/0.439_cleanup_defaults.sql
+++ db/updates/0.439_cleanup_defaults.sql
@@ -1,4 +1,6 @@
-﻿drop table defaults.lu_printer_host cascade;
+﻿drop table defaults.lu_link_printer_task;
+drop table defaults.lu_printer_host cascade;
 drop table defaults.lu_printer_task cascade;
 drop table defaults.temp;
+
 update db.lu_version set lu_minor=439;

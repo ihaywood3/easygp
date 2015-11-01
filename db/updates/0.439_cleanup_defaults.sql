cfab252700dd318a1a58732cc2780fd5351ba68e
Richard Terry <rterry@internode.on.net>
2015-11-01 03:29:05 +0000

    bugs

--- db/updates/0.439_cleanup_defaults.sql
+++ db/updates/0.439_cleanup_defaults.sql
@@ -1,4 +1,6 @@
-﻿drop table defaults.lu_link_printer_task;
+﻿dROP TABLE defaults.script_coordinates;
+drop table public.inventory_items_lent;
+drop table defaults.lu_link_printer_task;
 drop table defaults.lu_printer_host cascade;
 drop table defaults.lu_printer_task cascade;
 drop table defaults.temp;

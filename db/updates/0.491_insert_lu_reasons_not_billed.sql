f38ebb2dd05928570ed69a4e350410f11a8e0d02
Richard Terry <rterry@internode.on.net>
2016-03-14 10:46:10 +0000

    inserted some reasons why appointment was not billed

---
+++ db/updates/0.491_insert_lu_reasons_not_billed.sql
@@ -0,0 +1,13 @@
+﻿ALTER SEQUENCE billing.lu_reasons_not_billed_pk_seq RESTART WITH 1;
+
+insert into billing.lu_reasons_not_billed (reason) values ('duplicate appointment');
+insert into billing.lu_reasons_not_billed (reason) values ('patient DNA');
+insert into billing.lu_reasons_not_billed (reason) values ('no charge - after care');
+insert into billing.lu_reasons_not_billed (reason) values ('no charge - doctors choice unspecified');
+insert into billing.lu_reasons_not_billed (reason) values ('no charge - patient walked from consult');
+insert into billing.lu_reasons_not_billed (reason) values ('no charge - family member interview');
+insert into billing.lu_reasons_not_billed (reason) values ('no charge - traveller no insurance');
+
+
+update db.lu_version set lu_minor = 491;
+

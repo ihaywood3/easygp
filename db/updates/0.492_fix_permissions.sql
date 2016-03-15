2145b452b75764813ef2967a42f990ce851f7473
Horst Herb <hherb@dorrigomedical.com.au>
2016-03-15 04:01:01 +0000

    permissions fixed to avoid crash with centrelink certificates

--- db/updates/0.492_fix_permissions.sql
+++ db/updates/0.492_fix_permissions.sql
@@ -8,5 +8,13 @@ GRANT USAGE ON SEQUENCE clin_certificates.lu_fitness_pk_seq TO staff;
 GRANT USAGE ON SEQUENCE clin_certificates.lu_illness_temporality_pk_seq TO staff;
 GRANT USAGE ON SEQUENCE clin_certificates.medical_certificate_pk_seq TO staff;
 
+GRANT ALL ON SEQUENCE admin.lu_preferences_defaults_pk_seq TO staff;
+GRANT USAGE ON SEQUENCE admin.test_latex_templates_pk_seq TO staff;
+GRANT SELECT ON TABLE clin_measurements.vwlabinrs TO staff;
+GRANT USAGE ON SEQUENCE clin_recalls.lu_status_pk_seq TO staff;
+GRANT EXECUTE ON FUNCTION clerical.listsessions(timestamp without time zone, integer) TO staff;
+
+
+
 update db.lu_version set lu_minor = 492;
 

68182c956740049c56e0e7490f30c844d9aaa0e2
Richard Terry <rterry@internode.on.net>
2016-03-23 22:05:47 +0000

    mmm.. syntax error, sorry

--- db/updates/0.496_update_appointment_icons.sql
+++ db/updates/0.496_update_appointment_icons.sql
@@ -1,4 +1,4 @@
 ﻿insert into clerical.lu_appointment_icons(appointment_type, icon_path)
-values ('pap', 'icons/22/pap.png')
+values ('pap', 'icons/22/pap.png');
 
 update db.lu_version set lu_minor = 496;

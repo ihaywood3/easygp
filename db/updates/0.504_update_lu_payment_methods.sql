a2caf7cfaffbee870c16aa07623363896c2b6ac4
Richard Terry <rterry@internode.on.net>
2016-05-20 04:39:42 +0000

    Added ability to change payment type from popup menu over old accounts Changed background colour of daylist just for fun Another death-to-easygp bug in a popup menu in Prescriptions I've never seen before (fixed)

---
+++ db/updates/0.504_update_lu_payment_methods.sql
@@ -0,0 +1,5 @@
+﻿update billing.lu_payment_method set method = 'CASH' where pk=1;
+update billing.lu_payment_method set method = 'CHEQUE' where pk=3;
+update billing.lu_payment_method set method = 'MEDICARE' where pk=5;
+
+update db.lu_version set lu_minor = 504;

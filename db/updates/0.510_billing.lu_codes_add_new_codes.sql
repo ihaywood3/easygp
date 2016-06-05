667bbbe7859e800f25dffd2d420cadd3b6e521e6
Richard Terry <rterry@internode.on.net>
2016-06-05 04:00:11 +0000

    Work on billing - provision of bad debts Fixed function which updates invoice paid flag if billing items add up to total bill Changed a tab heading in preferences global

---
+++ db/updates/0.510_billing.lu_codes_add_new_codes.sql
@@ -0,0 +1,7 @@
+﻿-- new reason codes for failed itmes billed, to be used in the invoice GUI
+
+insert into billing.lu_codes (code, description) values (4020, 'Insurance refuses payment');
+insert into billing.lu_codes (code, description) values (4021, 'Patient refuses payment');
+
+update db.lu_version set lu_minor = 510;
+
---
+++ db/updates/0.511_fix_function_update_invoice_payment.sql
@@ -0,0 +1,25 @@
+﻿-- Function: billing.update_invoice_payment()
+
+-- DROP FUNCTION billing.update_invoice_payment();
+
+CREATE OR REPLACE FUNCTION billing.update_invoice_payment()
+  RETURNS trigger AS
+$BODY$
+    DECLARE
+       pk_invoice iNTEGER;
+    BEGIN
+        If (TG_OP = 'DELETE') THEN
+      pk_invoice = OLD.fk_invoice;
+   Else
+      pk_invoice = New .fk_invoice;
+   End If ;
+        update billing.invoices set total_paid = billing.invoice_received(pk) where pk = pk_invoice;
+   update billing.invoices set paid = (total_paid >= total_bill) where pk = pk_invoice;
+        Return Null; -- result Is Ignored since this Is An AFTER trigger
+    End ;
+$BODY$
+  LANGUAGE plpgsql VOLATILE
+  COST 100;
+ALTER FUNCTION billing.update_invoice_payment()   OWNER TO easygp;
+
+update db.lu_version set lu_minor = 511;

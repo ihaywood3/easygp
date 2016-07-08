dee2efd6c451c1e0b423371a2ac4b926a738c14e
Richard Terry <rterry@internode.on.net>
2016-07-08 12:31:43 +0000

    Did a litte work on GST report, a ?fix to the hl7 import where some loincs being missed

---
+++ db/updates/0.515_uppate_missing_hepatitis_loincs.sql
@@ -0,0 +1,9 @@
+﻿-- fixes missing loincs for hepatitis A/B serology
+
+update documents.observations set loinc = '20575-7' where identifier ILIKE 'hepatitis a igg'and loinc is null;
+update documents.observations set loinc = '75409-3' where identifier ILIKE 'hepatitis B Surface Ab'and loinc is null;
+
+--select * from documents.observations where identifier ILIKE 'hepatitis a igg'and loinc is null;
+-- select * from documents.observations where identifier ILIKE  'hepatitis B Surface Ab'and loinc is null;
+
+update db.lu_version set lu_minor=515;

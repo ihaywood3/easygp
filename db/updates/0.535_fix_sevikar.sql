0b59fe2ccc088c43d4392d0da7a611ea9145f4ca
richard.terry <richard.h.terry@gmail.com>
2017-10-30 09:39:56 +1100

    Left a 'dash' out of the comment line leading to crash on using this sql

--- db/updates/0.535_fix_sevikar.sql
+++ db/updates/0.535_fix_sevikar.sql
@@ -1,4 +1,4 @@
-- Something went awry in a previous drug update, the generic text went missing for sevikar HCT
+﻿-- Something went awry in a previous drug update, the generic text went missing for sevikar HCT
 
 update drugs.product set generic = 'olmesartan;amlodipine;hydrochlorothiazide' where pk = '0dacf19c-8d28-4879-b9dc-029e543bac30';
 update db.lu_version set lu_minor = 535;

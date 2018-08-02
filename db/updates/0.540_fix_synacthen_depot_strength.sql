627b002d9dc2ca9d2283c4d2aac9df60e998308f
richard.terry <richard.h.terry@gmail.com>
2018-08-02 17:41:27 +1000

    ooops, syntax error in update script (sorry)

--- db/updates/0.540_fix_synacthen_depot_strength.sql
+++ db/updates/0.540_fix_synacthen_depot_strength.sql
@@ -1,3 +1,3 @@
 ﻿-- drug database error
-update drugs.product set strength ='1mg/ml' where pk = 'ee790301-f0e5-4c3b-baee-34c332e35d8c' -- fix synacthen depot, was wrong strength (5mg/ml, should be 1mg/ml)
+update drugs.product set strength ='1mg/ml' where pk = 'ee790301-f0e5-4c3b-baee-34c332e35d8c'; -- fix synacthen depot, was wrong strength (5mg/ml, should be 1mg/ml)
 update db.lu_version set lu_minor=540;
